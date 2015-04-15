daa8169b3cc14024ec8ae5097b401446edd53e30
ahartwel <hartwellalex@gmail.com>
2015-04-15 19:27:52 -0400

    begin working with api

---
+++ redditCall.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$url = $_GET['url'];
+
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => "http://www." . $url,
+    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+
+echo $resp;
+
+
+?>
